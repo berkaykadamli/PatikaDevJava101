b4c57f90c6209337abba5de86db9e571d35f364a
berkaykadamli <berkaykadamli@gmail.com>
2021-06-16 17:46:49 +0300

    1

---
+++ Taksimetre/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int km;
+        double perKm = 2.20, total, startPrice = 10;
+        System.out.println("Km giriniz : ");
+        km = scan.nextInt();
+
+        total = (km * perKm);
+        total += startPrice;
+        total = (total < 20) ? 20 : total;
+        System.out.println("Toplam tutar : " + total);
+    }
+}
