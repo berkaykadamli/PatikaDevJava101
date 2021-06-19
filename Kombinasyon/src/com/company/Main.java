9bdf240bc1179bc6a8c212442707af2d1529e3f6
berkaykadamli <berkaykadamli@gmail.com>
2021-06-19 23:05:17 +0300

    Update Main.java

--- Kombinasyon/src/com/company/Main.java
+++ Kombinasyon/src/com/company/Main.java
@@ -6,26 +6,22 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int n, r, c = 1, n1 = 1, r1 = 1;
-        double total = 0;
+        int n, r, total;
         System.out.print("Kombinasyon için n : ");
         n = scan.nextInt();
         System.out.print("Kombinasyon için r : ");
         r = scan.nextInt();
-
-        for (int i = 1; i <= n; i++) {
-            n1 *= i;
-        }
-        for (int i = 1; i <= r; i++) {
-            r1 *= i;
-
-        }
-        for (int i = 1; i <= (n - r); i++) {
-            c *= i;
-        }
-        total = n1 / (r1 * c);
+        total = fact(n) / (fact(r) * fact(n - r));
         System.out.println("Kombinasyon : " + total);
 
 
+    }
+
+    static int fact(int n) {
+        int j = 1;
+        for (int i = 1; i <= n; i++) {
+            j *= i;
+        }
+        return j;
     }
 }
