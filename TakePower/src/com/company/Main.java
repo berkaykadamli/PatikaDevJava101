da2d3cdfb30d54fc68e85f358ec45e21f4811971
berkaykadamli <berkaykadamli@gmail.com>
2021-06-19 23:14:23 +0300

    Upload

---
+++ TakePower/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Taban sayı : ");
+        int n = scan.nextInt();
+        System.out.print("Kuvvet : ");
+        int r = scan.nextInt();
+        int total = 1;
+        for (int i = 0; i < r; i++) {
+            total *= n;
+        }
+        System.out.println("Kuvveti alınmış hali : " + total);
+
+
+    }
+}
