09692b886f9ad81bffd7cdc87b0a24f982aef106
berkaykadamli <berkaykadamli@gmail.com>
2021-06-21 13:01:21 +0300

    Update Main.java

--- YildizElmas/src/com/company/Main.java
+++ YildizElmas/src/com/company/Main.java
@@ -17,7 +17,7 @@ public class Main {
             }
             System.out.println(" ");
         }
-        
+
         for (int i = n - 1; i >= 0; i--) {
             for (int j = 0; j < (n - i); j++) {
                 System.out.print(" ");
