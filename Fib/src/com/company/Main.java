54df2ccb6430659c151497ffce21a372172c7789
berkaykadamli <berkaykadamli@gmail.com>
2021-06-24 02:01:13 +0300

    Add files via upload

---
+++ Fib/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Kaç elemanlı fib istiyorsun : ");
+        int n = scan.nextInt();
+        int n1 = 0, n2 = 1, n3;
+        System.out.print(n1 + " " + n2);
+        for (int i = 2; i <= n; i++) {
+            n3 = n1 + n2;
+            System.out.print(" "+n3 );
+            n1 = n2;
+            n2 = n3;
+
+        }
+    }
+
+}
